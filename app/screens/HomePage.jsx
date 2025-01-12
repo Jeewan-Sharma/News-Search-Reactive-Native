874efe464a8b9ce30d5c09ce35a443bc8498366e
Jeewan Sharma <sjeewan53@gmail.com>
2025-01-12 16:16:03 +1100

    Feat: Initial setup

---
+++ app/screens/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+function HomePage(props) {
+  return (
+    <View>
+      <Text>News Search</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default HomePage;
